e0e7fde80fb1e8728a93dcf6eee25d5f84e0b08a
Desi <pacifichik24@yahoo.com>
2021-06-14 17:37:34 -0700

    demo2java

---
+++ src/GithubDemo.java
@@ -0,0 +1,11 @@
+public class GithubDemo {
+
+
+
+    create new java class under src folder
+    make some changes
+    push to remote repository
+
+    1. git add *
+    2. git
+}
---
+++ src/demo2java.java
@@ -0,0 +1,13 @@
+public class demo2java {
+    /*
+
+            create new java class under src folder
+            make some changes
+            push to remote repository
+
+            1. git add *
+            2. git commit -m"your msg"
+            3. git push
+
+     */
+}
